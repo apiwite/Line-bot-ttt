1e57b740c0ecc9de64eff76aa544e59d496739e8
apiwite <95967543+apiwite@users.noreply.github.com>
2022-03-29 23:24:31 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type LineMessage struct {
+	Destination string `json:"destination"`
+	Events      []struct {
+		ReplyToken string `json:"replyToken"`
+		Type       string `json:"type"`
+		Timestamp  int64  `json:"timestamp"`
+		Source     struct {
+			Type   string `json:"type"`
+			UserID string `json:"userId"`
+		} `json:"source"`
+		Message struct {
+			ID   string `json:"id"`
+			Type string `json:"type"`
+			Text string `json:"text"`
+		} `json:"message"`
+	} `json:"events"`
+}
+
+type ReplyMessage struct {
+	ReplyToken string `json:"replyToken"`
+	Messages   []Text `json:"messages"`
+}
+
+type Text struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+type ProFile struct {
+	UserID        string `json:"userId"`
+	DisplayName   string `json:"displayName"`
+	PictureURL    string `json:"pictureUrl"`
+	StatusMessage string `json:"statusMessage"`
+}
+
+var ChannelToken = "CoUSULAzNPSSs/BPlXbqnkPAkhYYp3AxdaAxys/XZi2dDwUQvPuq6nvx/TDfVH43YdM1mrhHcnfMrHITchIHVNmaHW289I/SazoC/f++XLocDf7KZesDDO2VHH5LYwX+M3IjMhEGAQhWM4XYE7XjdAdB04t89/1O/w1cDnyilFU="
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ok")
+	})
+	e.POST("/webhook", func(c echo.Context) error {
+
+		Line := new(LineMessage)
+		if err := c.Bind(Line); err != nil {
+			log.Println("err")
+			return c.String(http.StatusOK, "error")
+		}
+
+		log.Println(Line)
+
+		fullname := getProfile(Line.Events[0].Source.UserID)
+
+		text := Text{
+			Type: "text",
+			Text: "ข้อความเข้ามา : " + Line.Events[0].Message.Text + " ยินดีต้อนรับ : " + fullname,
+		}
+
+		message := ReplyMessage{
+			ReplyToken: Line.Events[0].ReplyToken,
+			Messages: []Text{
+				text,
+			},
+		}
+
+		replyMessageLine(message)
+
+		log.Println("%% message success")
+		return c.String(http.StatusOK, "ok")
+
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func replyMessageLine(Message ReplyMessage) error {
+	value, _ := json.Marshal(Message)
+
+	url := "https://api.line.me/v2/bot/message/reply"
+
+	var jsonStr = []byte(value)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+ChannelToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil
+	}
+	defer resp.Body.Close()
+
+	log.Println("response Status:", resp.Status)
+	log.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Println("response Body:", string(body))
+
+	return err
+}
+
+func getProfile(userId string) string {
+
+	url := "https://api.line.me/v2/bot/profile/" + userId
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+ChannelToken)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var profile ProFile
+	if err := json.Unmarshal(body, &profile); err != nil {
+		log.Println("%% err \n")
+	}
+	log.Println(profile.DisplayName)
+	return profile.DisplayName
+
+}
